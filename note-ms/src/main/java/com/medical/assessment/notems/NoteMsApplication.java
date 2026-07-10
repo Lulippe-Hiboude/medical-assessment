237a3edfabc1eb146351172b74ce462ca4922223
Caroline de La Brosse <caroline.delabrosse@gmail.com>
2026-07-10 12:45:40 +0200

    chore(MA-17): initialize note-ms and mongoDB

---
+++ note-ms/src/main/java/com/medical/assessment/notems/NoteMsApplication.java
@@ -0,0 +1,13 @@
+package com.medical.assessment.notems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoteMsApplication.class, args);
+    }
+
+}
--- note-ms/src/test/java/com/medical/assessment/notems/NoteMsApplicationTests.java
+++ note-ms/src/test/java/com/medical/assessment/notems/NoteMsApplicationTests.java
@@ -1,12 +1,10 @@
-package com.medical.assessment.authms;
+package com.medical.assessment.notems;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("test")
-class AuthMsApplicationTests {
+class NoteMsApplicationTests {
 
     @Test
     void contextLoads() {
